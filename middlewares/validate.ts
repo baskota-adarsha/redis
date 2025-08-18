258686dfe5739b509080311fffc136578a3244a0
adarsha <022BSCIT005@sxc.edu.np>
2025-08-18 14:55:13 +0545

    zod and middleware

---
+++ middlewares/validate.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+export const validate =
+  <T>(schema: z.ZodType<T>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        success: false,
+
+        errors: result.error.issues,
+      });
+    }
+
+    next();
+  };
--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { validate } from "../middlewares/validate.js";
+import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 
 const router = express.Router();
 
-
-router.get
-
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
+  res.status(201).json({ message: "Got it" });
+});
 
 export default router;
