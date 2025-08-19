6969d5ae294b4e3b6ef55f0c28eb64cd1bda7951
adarsha <022BSCIT005@sxc.edu.np>
2025-08-19 14:54:42 +0545

    Hashes

---
+++ middlewares/checkRestaurantId.ts
@@ -0,0 +1,23 @@
+import type { Request, Response, NextFunction } from "express";
+import { initializeRedisClient } from "../utils/client.js";
+import { restaurantKeyById } from "../utils/keys.js";
+import { errorResponse } from "../utils/responses.js";
+export const checkRestaurantExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { restaurantId } = req.params;
+  if (!restaurantId) {
+    return errorResponse(res, 404, "Restaurant Id not found");
+  }
+
+  const client = await initializeRedisClient();
+  const restaurantKey = restaurantKeyById(restaurantId);
+  const exists = await client.exists(restaurantKey);
+
+  if (!exists) {
+    return errorResponse(res, 404, "Restaurant not found");
+  }
+  next();
+};
